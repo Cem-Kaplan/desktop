27acdf842ad5b83b90ff377d7dfcd50befb9c80d
Cem-Kaplan <cemk46908@gmail.com>
2025-12-25 13:28:07 +0100

    label hinzugefügt

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # desktop
 
+### App mit dem QT Framework in C++ hergestellt
+
 zum ausführen auf linux:
 
     g++ -fPIC main.cpp -o qt_app `pkg-config --cflags --libs Qt5Widgets`
--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QWidget>
+#include <QMessageBox>
+#include <QLabel>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     QWidget window;
-    window.setWindowTitle("Mein neues Fenster");
-    window.resize(500,400);
+    window.resize(1000,750);
+    window.setWindowTitle("Desktop App von Cem");
 
-    QPushButton button("Hallo Qt", &window);
+    QLabel label("In Bearbeitung", &window);
+    label.move(450, 300);
+    label.setStyleSheet("font-size: 17px");
+
     window.show();
+
 
     return app.exec();
 }
