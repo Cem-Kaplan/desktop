e9446851996ce5b61d482741650466cb1de2fad8
cm <cm@pop-os.speedport.ip>
2025-12-25 18:50:54 +0100

    added buttons

--- main.cpp
+++ main.cpp
@@ -12,10 +12,20 @@ int main(int argc, char *argv[])
     QWidget window;
     window.resize(1000,750);
     window.setWindowTitle("Desktop App von Cem");
-
+    
+    QLabel ueberschrift("Optionen", &window);
     QLabel label("In Bearbeitung", &window);
+    
+    QPushButton button1("Information", &window);
+    QPushButton button2("Information", &window);
+    
     label.move(450, 300);
     label.setStyleSheet("font-size: 17px");
+    ueberschrift.move(10, 10);
+    ueberschrift.setStyleSheet("font-size: 25px");
+
+    button1.move(10, 70);
+    button2.move(10, 120);
 
     window.show();
 
